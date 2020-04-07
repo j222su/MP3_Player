04be3559f118e119ff25c6726f3f6cc4abb388c6
infor15 <infor15@cien.co.kr>
2020-04-07 16:53:54 +0900

    add permission.WRITE_EXTERNAL_STORAGE

--- MP3Player/app/src/main/java/com/example/mp3player/MusicAdapter.java
+++ MP3Player/app/src/main/java/com/example/mp3player/MusicAdapter.java
@@ -1,4 +1,60 @@
 package com.example.mp3player;
 
-public class MusicAdapter {
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class MusicAdapter extends RecyclerView.Adapter<MusicAdapter.MusicViewHolder> {
+
+    int layout;
+    public ArrayList<MusicData>list;
+
+    public MusicAdapter(int layout, ArrayList<MusicData> list) {
+        this.layout = layout;
+        this.list = list;
+    }
+
+    @NonNull
+    @Override
+    public MusicViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(layout, parent, false);
+        MusicViewHolder musicViewHolder=new MusicViewHolder(view);
+        return musicViewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MusicViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return (list!=null)? list.size() : 0;
+    }
+
+    public class MusicViewHolder extends RecyclerView.ViewHolder {
+
+        ImageView imgAlbum;
+        TextView tvSinger;
+        TextView tvTitle;
+        TextView tvTotal;
+        ImageButton btnLike;
+
+        public MusicViewHolder(@NonNull View itemView) {
+            super(itemView);
+            imgAlbum=itemView.findViewById(R.id.imgAlbum);
+            tvSinger=itemView.findViewById(R.id.tvSinger);
+            tvTitle=itemView.findViewById(R.id.tvTitle);
+            tvTotal=itemView.findViewById(R.id.tvTotal);
+            btnLike=itemView.findViewById(R.id.btnLike);
+        }
+    }
 }
--- MP3Player/app/src/main/java/com/example/mp3player/MusicData.java
+++ MP3Player/app/src/main/java/com/example/mp3player/MusicData.java
@@ -1,4 +1,109 @@
 package com.example.mp3player;
 
 public class MusicData {
+    private String directory;
+    private String name;
+    private String artist;
+    private String album;
+    private String genre;
+    private String year;
+    private String duration;
+    private String track;
+    private String displayName;
+    private String albumArt;
+
+    public MusicData(String directory, String name, String artist, String album, String genre, String year, String duration, String track, String displayName, String albumArt) {
+        this.directory = directory;
+        this.name = name;
+        this.artist = artist;
+        this.album = album;
+        this.genre = genre;
+        this.year = year;
+        this.duration = duration;
+        this.track = track;
+        this.displayName = displayName;
+        this.albumArt = albumArt;
+    }
+
+    public String getDirectory() {
+        return directory;
+    }
+
+    public void setDirectory(String directory) {
+        this.directory = directory;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+    public String getAlbum() {
+        return album;
+    }
+
+    public void setAlbum(String album) {
+        this.album = album;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
+
+    public String getDuration() {
+        return duration;
+    }
+
+    public void setDuration(String duration) {
+        this.duration = duration;
+    }
+
+    public String getTrack() {
+        return track;
+    }
+
+    public void setTrack(String track) {
+        this.track = track;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    public void setDisplayName(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public String getAlbumArt() {
+        return albumArt;
+    }
+
+    public void setAlbumArt(String albumArt) {
+        this.albumArt = albumArt;
+    }
 }
+
+
