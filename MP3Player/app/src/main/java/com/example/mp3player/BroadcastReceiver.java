c02a06e9cda8610fd05ceb0c442e20c78885a554
infor15 <infor15@cien.co.kr>
2020-04-09 16:17:48 +0900

    add BroadcastReceiver.class

---
+++ MP3Player/app/src/main/java/com/example/mp3player/BroadcastReceiver.java
@@ -0,0 +1,11 @@
+package com.example.mp3player;
+
+import android.content.Context;
+import android.content.Intent;
+
+public class BroadcastReceiver extends android.content.BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        
+    }
+}
